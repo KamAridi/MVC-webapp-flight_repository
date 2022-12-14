7b8039eefa9c9c6a00282ce1671c92d14bb3d91f
KamAridi <aridi1994@mail.ru>
2022-12-14 20:33:22 +0300

    changing the way the client responds to jsp

--- src/com/servlet/FlightServlet.java
+++ src/com/servlet/FlightServlet.java
@@ -1,14 +1,13 @@
 package com.servlet;
 
 import com.service.FlightService;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
 
 @WebServlet("/flights")
 public class FlightServlet extends HttpServlet {
@@ -16,21 +15,9 @@ public class FlightServlet extends HttpServlet {
     private final FlightService flightService = FlightService.getInstance();
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
-        resp.setContentType("text/html");
-        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        req.setAttribute("flights", flightService.findAll());
+        req.getRequestDispatcher("WEB-INF/jsp/flights.jsp").forward(req, resp);
 
-        try (PrintWriter writer = resp.getWriter()) {
-            writer.write("<h1>Список перелетов</h1>");
-            writer.write("<ul>");
-            flightService.findAll().forEach(flightDto -> {
-                writer.write("""
-                        <li>
-                            <a href="/tickets?flightId=%d">%s</a>
-                        </li>
-                        """.formatted(flightDto.getId(), flightDto.getDescription()));
-            });
-            writer.write("</ul>");
-        }
     }
 }
